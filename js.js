588019554a4e2a5bae5c9beb43562a3b7687723f
Francisco <franciscoliveira7777@gmail.com>
2023-11-09 20:01:59 +0000

    added player moving animation

--- js.js
+++ js.js
@@ -1,4 +1,18 @@
 const timerDisplay = document.querySelector('.timer')
+const pauseWindow = document.querySelector('.pause-window')
+const body = document.querySelector('body')
+let isGameRunning = true
+
+const togglePauseGame = () => {
+    isGameRunning = !isGameRunning
+    if (isGameRunning) {
+        pauseWindow.style.display = 'none'
+        body.style.animationPlayState = 'running'
+    } else {
+        pauseWindow.style.display = 'flex'
+        body.style.animationPlayState = 'paused'
+    }
+}
 
 const startTimer = 10
 
@@ -14,50 +28,73 @@ const timer = setInterval(function () {
 }, 1000)
 
 let resetTimer = document.querySelector('.reset-timer')
-
 resetTimer.addEventListener('click', () => {
     count = startTimer
     timerDisplay.textContent = count
 })
 
+//? --- Player ---
+
 const Direction = {
     up: [0, -1],
     right: [1, 0],
     down: [0, 1],
-    left: [-1, 0]
+    left: [-1, 0],
 }
 
 class Player {
     constructor(x, y) {
         this.html = document.getElementById('player')
-        this.position = [x, y]
+        this.x = x
+        this.y = y
     }
-    updatePosition(direction, step) {
-        console.log(direction)
-        this.position[0] += direction[0]
-        this.position[1] += direction[1]
-        this.html.style.transform = `translate(${this.position[0] * step}rem, ${this.position[1] * step}rem)`
+    setPosition(direction, step) {
+        if (!this.checkPosition(direction)) return
+
+        /* animation: walk .6s linear infinite; */
+        this.x += direction[0]
+        this.y += direction[1]
+        this.html.style.transform = `translate(${this.x * step}rem,
+                ${this.y * step}rem)`
+    }
+    checkPosition(direction) {
+        return !(
+            this.x + direction[0] < 0 ||
+            this.x + direction[0] > 2 ||
+            this.y + direction[1] < 0 ||
+            this.y + direction[1] > 2
+        )
     }
 }
 
 let player = new Player(0, 0)
 
 document.addEventListener('keydown', (event) => {
-    let step = 10;
+    let step = 10
 
     switch (event.key) {
-        case 'w': case 'W':
-            player.updatePosition(Direction.up, step)
+        case 'w':
+        case 'W':
+        case 'ArrowUp':
+            player.setPosition(Direction.up, step)
             break
-        case 'a': case 'A':
-            player.updatePosition(Direction.left, step)
+        case 'a':
+        case 'A':
+        case 'ArrowLeft':
+            player.setPosition(Direction.left, step)
             break
-        case 's': case 'S':
-            player.updatePosition(Direction.down, step)
+        case 's':
+        case 'S':
+        case 'ArrowDown':
+            player.setPosition(Direction.down, step)
             break
-        case 'd': case 'D':
-            player.updatePosition(Direction.right, step)
+        case 'd':
+        case 'D':
+        case 'ArrowRight':
+            player.setPosition(Direction.right, step)
+            break
+        case 'Escape':
+            togglePauseGame()
             break
     }
-    console.log(event.key)
 })
